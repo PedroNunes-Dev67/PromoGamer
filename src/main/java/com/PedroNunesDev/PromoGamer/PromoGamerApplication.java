ae98d70919ad3e2134ed641b0f6cdeffdb25b222
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-04-22 13:47:50 -0300

    created: commit initial

---
+++ src/main/java/com/PedroNunesDev/PromoGamer/PromoGamerApplication.java
@@ -0,0 +1,13 @@
+package com.PedroNunesDev.PromoGamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoGamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PedroNunesDev/PromoGamer/PromoGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PedroNunesDev.PromoGamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
