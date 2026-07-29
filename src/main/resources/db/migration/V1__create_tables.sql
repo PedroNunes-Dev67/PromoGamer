0e7ade5af1304c41f5dabe8a11d79c74486a0b19
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-07-29 20:31:45 -0300

    feat: cria o create table da entidade Deal

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE deal (
+                      deal_id               VARCHAR(100)  NOT NULL,
+                      title                 VARCHAR(500)  NOT NULL,
+                      steam_app_id          VARCHAR(50)   NOT NULL,
+                      steam_rating_percent  VARCHAR(10)   NOT NULL ,
+                      status                VARCHAR(20)   NOT NULL DEFAULT 'PENDENTE',
+                      CONSTRAINT pk_deal PRIMARY KEY (deal_id)
+);
