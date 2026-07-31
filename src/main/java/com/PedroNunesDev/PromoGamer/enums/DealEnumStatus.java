2ffc3a6215241f4e9bc28defed4fbffe203268b1
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-07-31 17:47:51 -0300

    feat: cria método de busca do DealEnumStatus.java, passando uma String que verifica o tipo do enum e retorna caso ache

--- src/main/java/com/PedroNunesDev/PromoGamer/enums/DealEnumStatus.java
+++ src/main/java/com/PedroNunesDev/PromoGamer/enums/DealEnumStatus.java
@@ -2,7 +2,30 @@ package com.PedroNunesDev.PromoGamer.enums;
 
 public enum DealEnumStatus {
 
-    PENDENTE,
-    CONCLUIDO,
-    IGNORADO
+    PENDENTE("PENDENTE"),
+    CONCLUIDO("CONCLUIDO"),
+    IGNORADO("IGNORADO");
+
+    private String type;
+
+    DealEnumStatus(String type) {
+        this.type = type;
+    }
+
+    public static DealEnumStatus from(String typeStatusVerify){
+
+        DealEnumStatus dealEnumStatus = null;
+
+        for (DealEnumStatus dealEnumStatusObject : values()){
+            if (dealEnumStatusObject.type.equalsIgnoreCase(typeStatusVerify)){
+                dealEnumStatus = dealEnumStatusObject;
+            }
+        }
+
+        if (dealEnumStatus == null){
+            throw new IllegalArgumentException("Not found deal status");
+        }
+
+        return dealEnumStatus;
+    }
 }
