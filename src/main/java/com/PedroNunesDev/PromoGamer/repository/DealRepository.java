c8535738914f25e98c540bcb7afb68b78e90eaff
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-07-31 17:48:12 -0300

    feat: cria método de busca de Deal basedo em seu status

--- src/main/java/com/PedroNunesDev/PromoGamer/repository/DealRepository.java
+++ src/main/java/com/PedroNunesDev/PromoGamer/repository/DealRepository.java
@@ -1,10 +1,12 @@
 package com.PedroNunesDev.PromoGamer.repository;
 
+import com.PedroNunesDev.PromoGamer.enums.DealEnumStatus;
 import com.PedroNunesDev.PromoGamer.model.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Set;
 
 public interface DealRepository extends JpaRepository<Deal,String> {
@@ -14,4 +16,10 @@ public interface DealRepository extends JpaRepository<Deal,String> {
     WHERE d.dealId IN :ids
 """)
     Set<String> getExistingDealsId(@Param("ids") Set<String> dealsIdForSearching);
+
+    @Query("""
+    SELECT d FROM Deal d
+    WHERE d.dealEnumStatus = :enum
+""")
+    List<Deal> getAllDealsByDealEnumStatus(@Param("enum") DealEnumStatus dealEnumStatus);
 }
